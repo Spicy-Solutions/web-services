c22b0ac787d0993eec01212e3f21501b2f2f19a5
Mauricio <mauriciorojasanchez@gmail.com>
2025-11-03 15:41:25 -0500

    feat(iam): added credentials.

---
+++ SweetManagerWebService/IAM/Domain/Model/Entities/Credentials/AdminCredentialAudit.cs
@@ -0,0 +1,12 @@
+﻿using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SweetManagerWebService.IAM.Domain.Model.Entities.Credentials
+{
+    public partial class AdminCredential : IEntityWithCreatedUpdatedDate
+    {
+        [Column("created_at")] public DateTimeOffset? CreatedDate { get; set; }
+
+        [Column("updated_at")] public DateTimeOffset? UpdatedDate { get; set; }
+    }
+}
