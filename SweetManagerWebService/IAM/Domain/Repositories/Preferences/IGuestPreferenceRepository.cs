256d19dcd004a7d07a034ed66b945ab4cfa6db4f
Mauricio <mauriciorojasanchez@gmail.com>
2025-11-03 15:42:16 -0500

    feat(iam): added repositories.

---
+++ SweetManagerWebService/IAM/Domain/Repositories/Preferences/IGuestPreferenceRepository.cs
@@ -0,0 +1,11 @@
+﻿using SweetManagerWebService.IAM.Domain.Model.Entities.Preferences;
+using SweetManagerWebService.Shared.Domain.Repositories;
+
+namespace SweetManagerWebService.IAM.Domain.Repositories.Preferences
+{
+    public interface IGuestPreferenceRepository : IBaseRepository<GuestPreference>
+    {
+        Task<GuestPreference?> FindByGuestId(int guestId); 
+
+    }
+}
---
+++ SweetManagerWebService/IAM/Domain/Repositories/Users/IAdminRepository.cs
@@ -0,0 +1,16 @@
+﻿using SweetManagerWebService.IAM.Domain.Model.Aggregates;
+using SweetManagerWebService.Shared.Domain.Repositories;
+
+namespace SweetManagerWebService.IAM.Domain.Repositories.Users
+{
+    public interface IAdminRepository : IBaseRepository<Admin>
+    {
+        Task<dynamic?> FindAllByFiltersAsync(string? email, string? phone, string? state);
+
+        Task<IEnumerable<Admin>> FindAllByHotelIdAsync(int hotelId);
+
+        Task<int?> FindHotelIdByIdAsync(int id);
+
+        Task<bool> ExecuteUpdateAdminHotelIdAsync(int id, int hotelId);
+    }
+}
