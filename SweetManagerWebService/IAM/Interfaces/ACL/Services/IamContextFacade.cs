d2ac4a7cd4ed1dffb25d3c91dfdef67f726bb2df
Mauricio <mauriciorojasanchez@gmail.com>
2025-11-03 15:43:38 -0500

    feat(iam): added ACL.

---
+++ SweetManagerWebService/IAM/Interfaces/ACL/Services/IamContextFacade.cs
@@ -0,0 +1,55 @@
+﻿using SweetManagerWebService.IAM.Domain.Model.Aggregates;
+using SweetManagerWebService.IAM.Domain.Model.Entities.Preferences;
+using SweetManagerWebService.IAM.Domain.Model.Queries.Preferences;
+using SweetManagerWebService.IAM.Domain.Model.Queries.Users;
+using SweetManagerWebService.IAM.Domain.Services.QueryServices.Preferences;
+using SweetManagerWebService.IAM.Domain.Services.QueryServices.Users;
+
+namespace SweetManagerWebService.IAM.Interfaces.ACL.Services
+{
+    public class IamContextFacade(IAdminQueryService adminQueryService, IOwnerQueryService ownerQueryService,
+        IGuestPreferenceQueryService guestPreferenceQueryService) : IIamContextFacade
+    {
+        public async Task<Admin?> FetchAdminByUserId(int id)
+        {
+            try
+            {
+                var admin = await adminQueryService.Handle(new GetUserByIdQuery(id));
+
+                return admin;
+            }
+            catch(Exception)
+            {
+                return null;
+            }
+        }
+
+        public async Task<GuestPreference?> FetchGuestPreferenceById(int id)
+        {
+            try
+            {
+                var guestPreference = await guestPreferenceQueryService.Handle(new GetGuestPreferenceByGuestIdQuery(id));
+
+                return guestPreference;
+            }
+            catch(Exception)
+            {
+                return null;
+            }
+        }
+
+        public async Task<Owner?> FetchOwnerByUserId(int id)
+        {
+            try
+            {
+                var owner = await ownerQueryService.Handle(new GetUserByIdQuery(id));
+
+                return owner;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
